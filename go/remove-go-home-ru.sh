2ece2477d929413bcf3cf05ec06d708152ba6e53
OdaNoder <143608461+odanoder@users.noreply.github.com>
2024-02-23 18:02:05 +0200

    Update remove-go-home-ru.sh

--- go/remove-go-home-ru.sh
+++ go/remove-go-home-ru.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-# Удаляем директорию Go с правами суперпользователя
-sudo rm -rf "$HOME/go"
+# Проверяем наличие папки Go
+if [ -d "$HOME/go" ]; then
+    # Удаляем папку с правами суперпользователя
+    sudo rm -rf "$HOME/go"
+    
+    # Удаляем соответствующую строку из .bashrc
+    sudo sed -i '/export PATH=\$PATH:$HOME\/go\/bin/d' "$HOME/.bashrc"
 
-# Удаляем соответствующую строку из .bashrc
-sed -i '/export PATH=\$PATH:$HOME\/go\/bin/d' "$HOME/.bashrc"
-
-echo "Директория Go успешно удалена из домашней директории '${HOME}'."
+    echo "Go успешно удален из домашней директории '${HOME}'."
+else
+    echo "Папка Go не найдена в домашней директории '${HOME}'."
+fi
