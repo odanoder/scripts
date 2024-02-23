0983f3749afdc693f7d804a7479f20747474ed5d
OdaNoder <143608461+odanoder@users.noreply.github.com>
2024-02-23 17:47:06 +0200

    Create remove-go-home-en.sh

---
+++ go/remove-go-home-en.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Check if Go is installed
+if command -v go &>/dev/null; then
+    # Remove the Go directory
+    rm -rf "$HOME/go"
+
+    # Remove the corresponding line from .bashrc
+    sed -i '/export PATH=\$PATH:$HOME\/go\/bin/d' "$HOME/.bashrc"
+
+    echo "Go has been successfully removed from the home directory'${HOME}'."
+else
+    echo "Go was not found in the home directory '${HOME}'."
+fi
