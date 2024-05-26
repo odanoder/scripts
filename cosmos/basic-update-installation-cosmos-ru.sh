d5781cbb4734d1edbf859f71ec08b4fb1a850a8e
OdaNoder <143608461+odanoder@users.noreply.github.com>
2024-05-26 14:52:44 +0300

    Update basic-update-installation-cosmos-ru.sh

--- cosmos/basic-update-installation-cosmos-ru.sh
+++ cosmos/basic-update-installation-cosmos-ru.sh
@@ -5,28 +5,69 @@ error_message() {
     echo "Ошибка: $1"
 }
 
-# Функция для выполнения команды с проверкой на успешное выполнение
+# Функция для выполнения команды с проверкой на успешное выполнение и логированием
 execute_command() {
-    "$@"
-    local status=$?
+    "$@" | tee -a /tmp/package-log.txt
+    local status=${PIPESTATUS[0]}
     if [ $status -ne 0 ]; then
         error_message "Команда \"$@\" завершилась с ошибкой (код $status)."
         exit $status
     fi
 }
 
+# Инициализация лог файла
+> /tmp/package-log.txt
+
 # Обновление списка пакетов
 execute_command sudo apt update
 execute_command sudo apt upgrade -y
+
+# Списки пакетов для установки
+base_utils="coreutils nano htop wget curl git tmux"
+archivers="tar zip unzip lz4"
+dirs_disks="tree pstree ncdu nvme-cli"
+network="net-tools iptables"
+packages="pkg-config make gcc"
+cosmos="build-essential libssl-dev libleveldb-dev jq"
+
 # Установка пакетов: базовые утилиты
-execute_command sudo apt install coreutils nano htop wget curl git tmux pstree -y
+execute_command sudo apt install $base_utils -y
 # Установка пакетов: архиваторы
-execute_command sudo apt install tar zip lz4 -y
+execute_command sudo apt install $archivers -y
 # Установка пакетов: работа с директориями и дисками
-execute_command sudo apt install tree ncdu -y
+execute_command sudo apt install $dirs_disks -y
 # Установка пакетов: работа с сетью
-execute_command sudo apt install net-tools -y
+execute_command sudo apt install $network -y
 # Установка пакетов: работа с пакетами
-execute_command sudo apt install  gcc make -y
+execute_command sudo apt install $packages -y
 # Установка пакетов: для cosmos
-execute_command sudo apt install  build-essential libssl-dev libleveldb-dev jq -y
+execute_command sudo apt install $cosmos -y
+
+# Обработка и вывод логов
+all_packages="$base_utils $archivers $dirs_disks $network $packages $cosmos"
+installed_packages=$(grep -E "Setting up" /tmp/package-log.txt | awk '{print $2}')
+updated_packages=$(grep -E "upgraded" /tmp/package-log.txt | awk '{print $1}')
+already_installed_packages=$(grep -E "already the newest version" /tmp/package-log.txt | awk '{print $1}')
+
+echo "Все пакеты которые были в скрипте:"
+for pkg in $all_packages; do
+    echo "- $pkg"
+done
+
+echo ""
+echo "Обновленные пакеты:"
+for pkg in $updated_packages; do
+    echo "- $pkg"
+done
+
+echo ""
+echo "Установленные пакеты:"
+for pkg in $installed_packages; do
+    echo "- $pkg"
+done
+
+echo ""
+echo "Уже были установлены:"
+for pkg in $already_installed_packages; do
+    echo "- $pkg"
+done
