be47d77dbb9cd8b1c0fc2262899d0dc52fccd720
OdaNoder <143608461+odanoder@users.noreply.github.com>
2024-09-05 19:48:29 +0300

    Update install_fail2ban.sh

--- firewall/fail2ban/install_fail2ban.sh
+++ firewall/fail2ban/install_fail2ban.sh
@@ -11,17 +11,29 @@ sudo systemctl status fail2ban | grep "Active:"
 
 if sudo systemctl status fail2ban | grep -q "inactive (dead)"; then
     echo "Fail2Ban не запущен (inactive)."
+elif sudo systemctl status fail2ban | grep -q "active (running)"; then
+    echo "Fail2Ban запущен и работает (active)."
 else
-    echo "Fail2Ban запущен."
+    echo "Fail2Ban запущен, но с ошибками."
 fi
 
 echo "Создание резервной копии конфигурационного файла..."
-sudo cp /etc/fail2ban/jail.conf /etc/fail2ban/jail.local
-
-if [ -f /etc/fail2ban/jail.local ]; then
+if [ -f /etc/fail2ban/jail.conf ]; then
+    sudo cp /etc/fail2ban/jail.conf /etc/fail2ban/jail.local
     echo "Файл /etc/fail2ban/jail.local успешно создан."
 else
-    echo "Ошибка создания файла /etc/fail2ban/jail.local."
+    echo "Ошибка: файл /etc/fail2ban/jail.conf не найден."
 fi
 
-echo "Установка Fail2Ban завершена."
+# Дополнительное форматирование для выделения выводов
+echo -e "\n\n###**********###"
+echo "Проверка файлов конфигурации и статуса Fail2Ban:"
+
+if [ -f /etc/fail2ban/jail.local ]; then
+    echo "Файл /etc/fail2ban/jail.local существует."
+else
+    echo "Файл /etc/fail2ban/jail.local не найден."
+fi
+
+echo -e "\nПроверка статуса Fail2Ban..."
+sudo systemctl status fail2ban | grep "Active:"
