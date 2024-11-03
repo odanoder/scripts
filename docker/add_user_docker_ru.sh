d795a1085cfc1ddfebb70e0bdabbdf83d1e101ff
OdaNoder <143608461+odanoder@users.noreply.github.com>
2024-11-03 16:40:38 +0200

    Update add_user_docker_ru.sh

--- docker/add_user_docker_ru.sh
+++ docker/add_user_docker_ru.sh
@@ -1,16 +1,28 @@
 #!/bin/bash
 
+# Функция для добавления разделителя
+print_separator() {
+    echo "==============================================="
+}
+
 # Получаем текущее имя пользователя и сохраняем в переменную your_username
 your_username=$(whoami)
 
 # Выводим имя пользователя для подтверждения
-echo "Текущий пользователь: $your_username"
+print_separator
+echo -e "\e[1;34mТекущий пользователь:\e[0m \e[1;32m$your_username\e[0m"
+print_separator
 
 # Добавляем пользователя в группу docker
+echo -e "\e[1;33mДобавление пользователя $your_username в группу docker...\e[0m"
 sudo usermod -aG docker "$your_username"
 
 # Проверяем, что пользователь добавлен в группу docker
-echo "Пользователь $your_username добавлен в группу docker."
+print_separator
+echo -e "\e[1;32mУспешно:\e[0m Пользователь \e[1;32m$your_username\e[0m добавлен в группу \e[1;36mdocker\e[0m."
+print_separator
 
 # Предупреждение о необходимости перезагрузки
-echo "Для применения изменений выйдите из текущей сессии и войдите снова."
+echo -e "\e[1;31mВНИМАНИЕ:\e[0m Для применения изменений выйдите из текущей сессии и войдите снова."
+echo -e "Или выполните \e[1mnewgrp docker\e[0m для немедленного применения изменений."
+print_separator
