4e79bf4dfc67dd49d5cafdc8669a8d6c6f01ac94
OdaNoder <143608461+odanoder@users.noreply.github.com>
2025-02-16 21:35:42 +0200

    Update rust.sh

--- rust/rust.sh
+++ rust/rust.sh
@@ -4,3 +4,12 @@ sudo apt install curl make clang pkg-config libssl-dev build-essential git mc jq
 sudo curl https://sh.rustup.rs -sSf | sh -s -- -y
 source $HOME/.cargo/env
 sleep 5
+
+
+# Когда Rust устанавливается через скрипт sh -s -- -y, параметр -y автоматически принимает все стандартные настройки без необходимости ручного выбора.
+# По умолчанию Rust установится с default toolchain (stable) и добавит cargo, rustc, rustup в ~/.cargo/bin.
+# После выполнения source $HOME/.cargo/env они будут доступны в текущей сессии.
+# rustup show   Показать текущую версию
+# rustup update   Обновить Rust до актуальной версии
+# rustup default stable  Назначить стабильную версию по умолчанию
+# rustup default nightly Переключиться на nightly-версию
