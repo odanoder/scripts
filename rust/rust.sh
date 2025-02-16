a2e407efd4982a0ef4c98c6e1a4b288ccf9a761b
OdaNoder <143608461+odanoder@users.noreply.github.com>
2025-02-16 21:31:25 +0200

    Update rust.sh

--- rust/rust.sh
+++ rust/rust.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo apt update
+sudo apt update -y
 sudo apt install curl make clang pkg-config libssl-dev build-essential git mc jq unzip wget -y
 sudo curl https://sh.rustup.rs -sSf | sh -s -- -y
 source $HOME/.cargo/env
