ca18dda3e6d1ef8f45c14823915a4c18decd97a8
OdaNoder <143608461+odanoder@users.noreply.github.com>
2025-04-01 19:00:20 +0300

    Update npm_check_install.sh

--- other/npm_check_install.sh
+++ other/npm_check_install.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+# Цвета
+RED='\033[0;31m'
+NC='\033[0m' # Сброс цвета
+
 # Проверка наличия npm
 if command -v npm &> /dev/null
 then
-    echo "npm уже установлен. Версия: $(npm -v)"
+    echo -e "\n${RED}npm установлен. Версия: $(npm -v)${NC}\n"
 else
     echo "npm не найден. Устанавливаем..."
     sudo apt update && sudo apt install -y npm
-    echo "npm установлен. Версия: $(npm -v)"
+    echo -e "\n${RED}npm установлен. Версия: $(npm -v)${NC}\n"
 fi
